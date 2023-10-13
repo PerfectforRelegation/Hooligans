a1440e8a7a451a9b5cc62402905289f05fce68a2
JosephSeong <joseph0903@naver.com>
2023-10-13 16:57:53 +0900

    animation complemting..

--- Hooligans/Hooligans/Scenes/Board/BoardListViewController.swift
+++ Hooligans/Hooligans/Scenes/Board/BoardListViewController.swift
@@ -1,29 +1,120 @@
 //
-//  BoardListViewController.swift
-//  Hooligans
+//import UIKit
 //
-//  Created by Joseph on 2023/10/10.
+////protocol SigninDisplayLogic: AnyObject {
+////    func displayAnything(viewModel: SigninModels.BoardContents.ViewModel)
+////}
 //
-
-import UIKit
-
-class BoardListViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+//class BoardListViewController: UIViewController {
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Do any additional setup after loading the view.
+//    }
+//
+//
+//
+//}
+//
+//    //var interactor: (SigninBusinessLogic & SigninDataStore)?
+//
+//    var users: [User]?
+//    var boardContents: [Board]?
+//
+//    private let tableView: UITableView = {
+//        let table = UITableView()
+//        table.translatesAutoresizingMaskIntoConstraints = false
+//        table.frame = CGRect(origin: .zero, size: .zero)
+//        table.backgroundColor = .black
+//        return table
+//    }()
+//
+//    init() {
+//        super.init(nibName: nil, bundle: nil)
+//        setup()
+//    }
+//
+//    func setup() {
+//        let viewController = self
+//        let interactor = SigninInteractor()
+//        let presenter = SigninPresenter()
+//        _ = SigninRouter()
+//        viewController.interactor = interactor
+////        viewController.router = router
+//        interactor.presenter = presenter
+//        presenter.viewController = viewController
+////        router.viewController = viewController
+//    }
+//
+//
+//
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        self.view.backgroundColor = .yellow
+//        tableView.delegate = self
+//        tableView.dataSource = self
+//        setupView()
+//        registerCells()
+//        // Do any additional setup after loading the view.
+//    }
+//
+//    func setupd() {
+//        let viewController = self
+//        let interactor = SigninInteractor()
+//        let presenter = SigninPresenter()
+//        _ = SigninRouter()
+//        viewController.interactor = interactor
+////        viewController.router = router
+//        interactor.presenter = presenter
+//        presenter.viewController = viewController
+////        router.viewController = viewController
+//    }
+//
+//    func registerCells() {
+//        tableView.register(TestTableViewCell.self, forCellReuseIdentifier: TestTableViewCell.identifier)
+//    }
+//
+//}
+//
+//
+//extension SigninViewController {
+//    private func setupView() {
+//        self.view.addSubview(tableView)
+//
+//        tableView.snp.makeConstraints { make in
+//            make.top.leading.trailing.bottom.equalToSuperview()
+//        }
+//    }
+//}
+//
+//extension SigninViewController: SigninDisplayLogic {
+//    func displayAnything(viewModel: SigninModels.BoardContents.ViewModel) {
+//        DispatchQueue.main.async {
+//            self.boardContents = viewModel.boardContents
+//            self.tableView.reloadData()
+//        }
+//    }
+//}
+//
+//
+//extension SigninViewController: UITableViewDelegate, UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return self.boardContents?.count ?? 0
+//    }
+//
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 150
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        guard let cell = tableView.dequeueReusableCell(withIdentifier: SigninTableViewCell.identifier, for: indexPath) as? SigninTableViewCell else { return UITableViewCell() }
+//                cell.label.text = self.users?[indexPath.row].name
+//                return cell
+//
+//    }
+//}
