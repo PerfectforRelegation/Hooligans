42df0c9ce1de0784bdee01aa8412355c0983b1d0
JosephSeong <joseph0903@naver.com>
2023-10-15 01:48:12 +0900

    boardlist struct

--- Hooligans/Hooligans/Scenes/Board/BoardListViewController.swift
+++ Hooligans/Hooligans/Scenes/Board/BoardListViewController.swift
@@ -1,120 +1,107 @@
+
+import UIKit
+import SnapKit
+
+protocol BoardListDisplayLogic: AnyObject {
+    func displayA(viewModel: BoardListModels.PostContents.ViewModel)
+}
+
+class BoardListViewController: UIViewController {
+
+    var interactor: (BoardListBusinessLogic & BoardListDataStore)?
+
+    var postList: [Post]?
+
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.frame = CGRect(origin: .zero, size: .zero)
+        table.backgroundColor = .black
+        return table
+    }()
+
+        init() {
+            super.init(nibName: nil, bundle: nil)
+            setup()
+        }
+
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+
+    func setup() {
+        let viewController = self
+        let interactor = BoardListInteractor()
+        let presenter = BoardListPresenter()
+        _ = BoardListRouter()
+        viewController.interactor = interactor
+        //        viewController.router = router
+        interactor.presenter = presenter
+        presenter.viewController = viewController
+        //        router.viewController = viewController
+    }
+
+    //    override func viewDidLoad() {
+    //        super.viewDidLoad()
+    //        self.view.backgroundColor = .yellow
+    //        tableView.delegate = self
+    //        tableView.dataSource = self
+    //        setupView()
+    //        registerCells()
+    //
+    //    }
+
+    //    func setupd() {
+    //        let viewController = self
+    //        let interactor = BoardListInteractor()
+    //        let presenter = BoardListPresenter()
+    //        _ = BoardListRouter()
+    //        viewController.interactor = interactor
+    ////        viewController.router = router
+    //        interactor.presenter = presenter
+    //        presenter.viewController = viewController
+    ////        router.viewController = viewController
+    //    }
+
+    func registerCells() {
+        tableView.register(TestTableViewCell.self, forCellReuseIdentifier: TestTableViewCell.identifier)
+    }
+
+}
+
+extension BoardListViewController {
+        private func setupView() {
+            self.view.addSubview(tableView)
+
+            tableView.snp.makeConstraints { make in
+                make.top.leading.trailing.bottom.equalToSuperview()
+            }
+        }
+    }
+
+extension BoardListViewController: BoardListDisplayLogic {
+        func displayA(viewModel: BoardListModels.PostContents.ViewModel) {
+            DispatchQueue.main.async {
+                self.postList = viewModel.postContents
+                self.tableView.reloadData()
+            }
+        }
+}
+
+
+//extension BoardListViewController: UITableViewDelegate, UITableViewDataSource {
+//        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//            return self.postList?.count ?? 0
+//        }
 //
-//import UIKit
+//        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//            return 150
+//        }
 //
-////protocol SigninDisplayLogic: AnyObject {
-////    func displayAnything(viewModel: SigninModels.BoardContents.ViewModel)
-////}
+//        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//            guard let cell = tableView.dequeueReusableCell(withIdentifier: BoardListTableViewCell.identifier, for: indexPath) as? BoardListTableViewCell else { return UITableViewCell() }
+//            cell.label.text = self.users?[indexPath.row].name
+//            return cell
 //
-//class BoardListViewController: UIViewController {
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        // Do any additional setup after loading the view.
-//    }
-//
-//
-//
-//}
-//
-//    //var interactor: (SigninBusinessLogic & SigninDataStore)?
-//
-//    var users: [User]?
-//    var boardContents: [Board]?
-//
-//    private let tableView: UITableView = {
-//        let table = UITableView()
-//        table.translatesAutoresizingMaskIntoConstraints = false
-//        table.frame = CGRect(origin: .zero, size: .zero)
-//        table.backgroundColor = .black
-//        return table
-//    }()
-//
-//    init() {
-//        super.init(nibName: nil, bundle: nil)
-//        setup()
-//    }
-//
-//    func setup() {
-//        let viewController = self
-//        let interactor = SigninInteractor()
-//        let presenter = SigninPresenter()
-//        _ = SigninRouter()
-//        viewController.interactor = interactor
-////        viewController.router = router
-//        interactor.presenter = presenter
-//        presenter.viewController = viewController
-////        router.viewController = viewController
-//    }
-//
-//
-//
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        self.view.backgroundColor = .yellow
-//        tableView.delegate = self
-//        tableView.dataSource = self
-//        setupView()
-//        registerCells()
-//        // Do any additional setup after loading the view.
-//    }
-//
-//    func setupd() {
-//        let viewController = self
-//        let interactor = SigninInteractor()
-//        let presenter = SigninPresenter()
-//        _ = SigninRouter()
-//        viewController.interactor = interactor
-////        viewController.router = router
-//        interactor.presenter = presenter
-//        presenter.viewController = viewController
-////        router.viewController = viewController
-//    }
-//
-//    func registerCells() {
-//        tableView.register(TestTableViewCell.self, forCellReuseIdentifier: TestTableViewCell.identifier)
-//    }
-//
-//}
-//
-//
-//extension SigninViewController {
-//    private func setupView() {
-//        self.view.addSubview(tableView)
-//
-//        tableView.snp.makeConstraints { make in
-//            make.top.leading.trailing.bottom.equalToSuperview()
 //        }
 //    }
-//}
-//
-//extension SigninViewController: SigninDisplayLogic {
-//    func displayAnything(viewModel: SigninModels.BoardContents.ViewModel) {
-//        DispatchQueue.main.async {
-//            self.boardContents = viewModel.boardContents
-//            self.tableView.reloadData()
-//        }
-//    }
-//}
-//
-//
-//extension SigninViewController: UITableViewDelegate, UITableViewDataSource {
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return self.boardContents?.count ?? 0
-//    }
-//
-//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        return 150
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        guard let cell = tableView.dequeueReusableCell(withIdentifier: SigninTableViewCell.identifier, for: indexPath) as? SigninTableViewCell else { return UITableViewCell() }
-//                cell.label.text = self.users?[indexPath.row].name
-//                return cell
-//
-//    }
-//}
--- Hooligans/Hooligans/Service/APIService.swift
+++ Hooligans/Hooligans/Service/APIService.swift
@@ -26,6 +26,8 @@ class APIService {
     let request = get(url: Endpoint.userList.url)
 
     let requestBoardList = get(url: Endpoint.boardList.url)
+
+    let requestPostList = get(url: Endpoint.postList.url)
     
     // 직접적으로 서버에게 api 요청, JSON 형태의 응답을 구조체(struct, 여기선 Board)로 디코딩(decode)
     func fetchUsers(completion: @escaping (Result<[User], APIError>) -> Void) {
@@ -62,6 +64,27 @@ class APIService {
             }
 
             completion(.success(boardList))
+
+        }.resume()
+
+    }
+
+
+    func postList(completion: @escaping (Result<[Post], APIError>) -> Void) {
+
+            // 엔드포인트 작성 후 적용
+            session.dataTask(with: Endpoint.postList.url) { data, response, error in
+
+            guard let data = data else {
+                return completion(.failure(.data))
+            }
+
+            guard let postList = try? JSONDecoder().decode([Post].self, from: data) else {
+                print(data.description)
+                return completion(.failure(.decodingJSON))
+            }
+
+            completion(.success(postList))
 
         }.resume()
 
